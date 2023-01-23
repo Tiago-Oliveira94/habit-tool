c698cfa34aa2b27f709ca33467e80aa6dae69a38
Tiago-Oliveira94 <ti.oliprado@gmail.com>
2023-01-23 18:33:21 -0300

    initial setup for backend, web and mobile

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const app = Fastify()
+app.register(cors)
+const prisma = new PrismaClient()
+
+app.get('/habits', () => {
+    const habits = prisma.habit.findMany()
+    return habits
+ })
+const port = 3333
+
+app.listen({
+    port: port
+}).then(() => { console.log(`Server is running on port ${port}!`)})
