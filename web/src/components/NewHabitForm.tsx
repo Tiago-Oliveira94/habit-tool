406bd21910833d99ca667b223a0dbf7791bcf8b5
Tiago-Oliveira94 <ti.oliprado@gmail.com>
2023-03-16 12:43:32 -0300

    change frontend confirm and checkbox components color to violet

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -66,7 +66,7 @@ export function NewHabitForm() {
                             onCheckedChange={() => handleToggleWeekDays(index)}
                         >
 
-                            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
+                            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-violet-600 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
                                 <Checkbox.Indicator>
                                     <Check
                                         size={20}
@@ -84,7 +84,7 @@ export function NewHabitForm() {
 
             </div>
 
-            <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors">
+            <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-violet-600 hover:bg-violet-500 transition-colors">
                 <Check size={20} weight="bold" />
                 Confirm
             </button>
